3f4e1c94edcfa872498420a77cd0daa563f51921
RorshaR <panzeriii@hotmail.fr>
2026-07-25 09:03:10 +0200

    Change menu to improve function
    Add a function to enter programing mode insted of continuous menus

--- FCU-Wolverine/Menu.h
+++ FCU-Wolverine/Menu.h
@@ -14,10 +14,9 @@ public:
 	void SniperMenu(uint8_t timeBolt, bool greenLed, uint8_t subMenuValue);
 	void MagMenu1(uint16_t chargeurCap, bool switchchargeur, bool buzzer, bool blocage, uint8_t subMenuValue);
 	void MagMenu2(bool alarm, uint8_t alarmLowBB, bool chargingHandle, bool greenLight, uint8_t subMenuValue);
-	void SettingsMenu1(uint8_t veille, bool verrou, bool LowBat, uint8_t subMenuValue);
-	void SettingMenu2(uint8_t semi, uint8_t full, uint16_t dwel, uint8_t subMenuValue);
-	void UnlockDisplay();
-	void lockDisplay();
+	void SettingMenu(uint8_t semi, uint8_t full, uint16_t dwel, bool LowBat, uint8_t subMenuValue);
+	void SettingsSaved();
+	void EnterProgMode();
 };
 
 #endif
